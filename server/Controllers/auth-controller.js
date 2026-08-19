fb7b09978119a77e8c58d23f66e7e0ed45d56a15
Nitin Singh <nitin_2312res424@iitp.ac.in>
2026-08-19 20:01:36 +0530

    port change 587

--- server/Controllers/auth-controller.js
+++ server/Controllers/auth-controller.js
@@ -7,12 +7,15 @@ const { JWT_SECRET, EMAIL, PASSWORD } = require("../secrets.js");
 
 let mailTransporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
-  port: 465,
-  secure: true,
+  port: 587,
+  secure: false,
   family: 4,
   auth: {
     user: EMAIL,
     pass: PASSWORD,
+  },
+  tls: {
+    rejectUnauthorized: false
   },
   connectionTimeout: 120000,
   greetingTimeout: 120000,
--- server/utils/sendMessageEmail.js
+++ server/utils/sendMessageEmail.js
@@ -15,10 +15,13 @@ const { EMAIL, PASSWORD, FRONTEND_URL } = require("../secrets.js");
 
 const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
+    port: 587,
+    secure: false,
     family: 4,
     auth: { user: EMAIL, pass: PASSWORD },
+    tls: {
+      rejectUnauthorized: false
+    },
     connectionTimeout: 120000,
     greetingTimeout: 120000,
     socketTimeout: 120000,
